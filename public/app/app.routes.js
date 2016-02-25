274d415403ffc5716692f547c4ac9a21f1796a1e
Patrick Moore <adjunkd@gmail.com>
2016-02-24 21:59:32 -0800

    Initial Angular Frontend

---
+++ public/app/app.routes.js
@@ -0,0 +1,33 @@
+angular.module('app.routes', ['ngRoute'])
+.config(function($routeProvider, $locationProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl: 'app/views/pages/home.html'
+  })
+
+  .when('/login', {
+    templateUrl: 'app/views/pages/login.html',
+    controller: 'mainController',
+    controllerAs: 'login'
+  })
+
+  .when('/users', {
+    templateUrl: 'app/views/pages/users/all.html',
+    controller: 'userController',
+    controllerAs: 'user'
+  })
+
+  .when('/users/create', {
+    templateUrl: 'app/views/pages/users/single.html',
+    controller: 'userCreateController',
+    controllerAs: 'user'
+  })
+
+  .when('/users/:user_id', {
+    templateUrl: 'app/views/pages/users/single.html',
+    controller: 'userEditController',
+    controllerAs: 'user'
+  });
+
+  $locationProvider.html5Mode(true);
+});
---
+++ public/app/services/authService.js
@@ -0,0 +1,92 @@
+angular.module('authService', [])
+
+// Login and get user information
+.factory('Auth', function($http, $q, AuthToken) {
+  var authFactory = {};
+
+  // Login user
+  authFactory.login = function(username, password) {
+    return $http.post('/api/authenticate', {
+        username: username,
+        password: password
+      })
+      .success(function(data) {
+        AuthToken.setToken(data.token);
+        return data;
+      });
+  };
+
+  // Logout user
+  authFactory.logout = function() {
+    AuthToken.setToken();
+  };
+
+  // Check if user is logged in via local token
+  authFactory.isLoggedIn = function() {
+    if (AuthToken.getToken()) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // Get the logged in user
+  authFactory.getUser = function() {
+    if (AuthToken.getToken()) {
+      return $http.get('/api/me', { cache: true });
+    } else {
+      return $q.reject({
+        message: 'User has no token.'
+      });
+    }
+  };
+
+  return authFactory;
+})
+
+// Getting and setting token
+.factory('AuthToken', function($window) {
+  var authTokenFactory = {};
+
+  // get token out of local storage
+  authTokenFactory.getToken = function() {
+    return $window.localStorage.getItem('token');
+  };
+
+  // Set and clear token
+
+  authTokenFactory.setToken = function(token) {
+    if (token) {
+      $window.localStorage.setItem('token', token);
+    } else {
+      $window.localStorage.removeItem('token');
+    }
+  };
+
+  return authTokenFactory;
+})
+
+// Integrate token into requests
+.factory('AuthInterceptor', function($q, $location, AuthToken) {
+  var interceptorFactory = {};
+
+  interceptorFactory.request = function(config) {
+    var token = AuthToken.getToken();
+
+    if (token) {
+      config.headers['x-access-token'] = token;
+    }
+    return config;
+  };
+
+  interceptorFactory.responseError = function(response) {
+    if (response.status == 403) {
+      AuthToken.setToken();
+      $location.path('/login');
+    }
+
+    return $q.reject(response);
+  };
+
+  return interceptorFactory;
+});
