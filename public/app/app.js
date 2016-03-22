119c5c0f1bda06c85df718ac8270d792fcee21cf
Patrick Moore <adjunkd@gmail.com>
2016-03-21 21:37:43 -0400

    Replace Bootstrap JS with UI Bootstrap

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('userApp', ['ngAnimate', 'ngSanitize', 'app.routes', 'authService', 'profileCtrl', 'mainCtrl', 'userCtrl', 'feedCtrl', 'adminCtrl', 'userService', 'subscriptionService', 'subscriptionCtrl'])
+angular.module('userApp', ['ngAnimate', 'ngSanitize','ui.bootstrap', 'app.routes', 'authService', 'profileCtrl', 'mainCtrl', 'userCtrl', 'feedCtrl', 'adminCtrl', 'userService', 'subscriptionService', 'subscriptionCtrl'])
   .config(function($httpProvider) {
     // Attach intercpetor to http requests
     $httpProvider.interceptors.push('AuthInterceptor');
