180c208730584f9b9390f70029f22ec963ff1bc9
Patrick Moore <adjunkd@gmail.com>
2016-04-11 17:41:52 -0700

    Correct App Name to Oyster

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('userApp', ['ngAnimate', 'ngSanitize','ui.bootstrap', 'app.routes', 'authService', 'profileCtrl', 'mainCtrl', 'userCtrl', 'subscriptionCtrl', 'feedCtrl', 'feedService', 'adminCtrl', 'userService', 'subscriptionService', 'subscriptionCtrl'])
+angular.module('Oyster', ['ngAnimate', 'ngSanitize','ui.bootstrap', 'app.routes', 'authService', 'profileCtrl', 'mainCtrl', 'userCtrl', 'subscriptionCtrl', 'feedCtrl', 'feedService', 'adminCtrl', 'userService', 'subscriptionService', 'subscriptionCtrl'])
   .config(function($httpProvider) {
     // Attach intercpetor to http requests
     $httpProvider.interceptors.push('AuthInterceptor');
