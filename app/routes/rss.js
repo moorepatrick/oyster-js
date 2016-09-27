8548cfd10cb7cecb64b6dfadc99b25e597e9b426
Patrick Moore <adjunkd@gmail.com>
2016-09-26 22:30:31 -0700

    routes/api: Add rss feed generation timer

--- app/routes/rss.js
+++ app/routes/rss.js
@@ -6,53 +6,72 @@ var OutputFeed = require('../models/feed').outputFeed,
 
 module.exports = function(app, express) {
 
-var rssRouter = express.Router();
+  var rssRouter = express.Router();
 
-// RSS Feed
-rssRouter.route('/:username/:feed_id')
-  .get(function(req, res) {
-    // Source Feed Regeneration
-    // Split off file extension if present
-    var feedId = req.params.feed_id.split('.')[0];
-    var filePath = path.join(__dirname, '../../_public/feeds/' + req.params.username);
-    mkdirp.sync(filePath, function(err) {
-      if (err) {
-        console.err(err);
-        res.json(err);
-      }
-    });
-    console.log(filePath);
-    OutputFeed.findOne({ normTitle: feedId })
-      .populate({ path: 'articles', select: '-_id', populate: { path: 'article', select: '-_id -meta' } })
-      .exec(function(err, feed) {
-        if (!feed) return res.json({ message: "Feed Not Found" });
+  // RSS Feed
+  rssRouter.route('/:username/:feed_id')
+    .get(function(req, res) {
+      var timer1 = Date.now();
 
-        if (Date.parse(feed.lastBuildDate) < (Date.now() - 900000)) {
-          console.log("Old Feed: > 15 mins since last build");
+      // Source Feed Regeneration
+      // Split off file extension if present
+      var feedId = req.params.feed_id.split('.')[0];
+      var filePath = path.join(__dirname, '../../_public/feeds/' + req.params.username);
+      mkdirp.sync(filePath, function(err) {
+        if(err) {
+          console.err(err);
+          res.json(err);
+        }
+      });
+      console.log(filePath);
+      OutputFeed.findOne({
+          normTitle: feedId
+        })
+        .populate({
+          path: 'articles',
+          select: '-_id',
+          populate: {
+            path: 'article',
+            select: '-_id -meta'
+          }
+        })
+        .exec(function(err, feed) {
+          if(!feed) return res.json({
+            message: "Feed Not Found"
+          });
+
+          if(Date.parse(feed.lastBuildDate) < (Date.now() - 900000)) {
+            console.log("Old Feed: > 15 mins since last build");
             // console.log("Last Build Date: " + feed.lastBuildDate);
             // console.log("Date Now: " + new Date(Date.now()));
             // console.log("Difference:" + (Date.parse(feed.lastBuildDate) - (Date.now() - 900000)))
 
-          rssCtrl.generateFeed(feed, req.params.username)
-            .then(function(data) {
-              console.log(data);
-              feed.lastBuildDate = new Date(data.date);
-              feed.save(function(err) {
-                if (err) return err;
-                res.sendFile(path.join(filePath, feedId + ".xml"));
+            rssCtrl.generateFeed(feed, req.params.username)
+              .then(function(data) {
+                console.log(data);
+                feed.lastBuildDate = new Date(data.date);
+                feed.save(function(err) {
+                  if(err) return err;
+                  res.sendFile(path.join(filePath, feedId + ".xml"));
+                  var timer2 = Date.now()
+                  console.log("Time: " + (timer2 - timer1) + "ms");
+
+                });
+              })
+              .catch(function(data) {
+                //console.log(util.inspect(data));
+                res.json(data);
               });
-            })
-            .catch(function(data) {
-              //console.log(util.inspect(data));
-              res.json(data);
-            });
 
-        } else {
-          console.log("Fresh Feed: < 15 mins since last build");
-          res.sendFile(path.join(filePath, feedId + ".xml"));
-        }
-      });
-  });
+          } else {
+            console.log("Fresh Feed: < 15 mins since last build");
+            res.sendFile(path.join(filePath, feedId + ".xml"));
+            var timer2 = Date.now()
+            console.log("Time: " + (timer2 - timer1) + "ms");
 
-return rssRouter;
+          }
+        });
+    });
+
+  return rssRouter;
 };
